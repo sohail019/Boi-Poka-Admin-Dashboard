cb0668cac74ce6def92acd4936eb114efe7ae11e
Aniket Sharma <89375574+aniketsham@users.noreply.github.com>
2025-01-06 11:45:07 +0530

    feat: add Poppins font to the project and clean up CSS formatting

--- src/utils/axios-instance.ts
+++ src/utils/axios-instance.ts
@@ -7,18 +7,18 @@ const axiosInstance = axios.create({
   },
 });
 
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     const token = useSelector((state: RootState) => state.auth.token);
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
 
 // axiosInstance.interceptors.response.use(
 //   (response) => response,
